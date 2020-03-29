f7315c910ddda94c0c6d3ba228c815aa80388203
Anish Badri R S <anishbadhri@gmail.com>
2020-03-29 20:03:21 +0530

    Added time taken

--- src/interpreter/Interpreter.cpp
+++ src/interpreter/Interpreter.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include "Interpreter.h"
 
 void Interpreter::interpret(const std::string& input) {
@@ -5,7 +6,6 @@ void Interpreter::interpret(const std::string& input) {
     quit.perform();
   } else if(bound.parse(input)){
     std::string res = bound.perform();
-//        std::cout << res << std::endl;
   } else if(length.parse(input)) {
     std::string res = length.perform();
     std::cout << res << std::endl;
@@ -16,14 +16,22 @@ void Interpreter::interpret(const std::string& input) {
     common_verifier = &ltl_bmc_verifier;
     trace.setVerifier(&ltl_bmc_verifier);
     length.setVerifier(&ltl_bmc_verifier);
+    auto start = std::chrono::high_resolution_clock::now();
     std::string result = ltlspec.perform();
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
     std::cout << result << std::endl;
+    std::cout << "Time taken: " << duration.count()/1e6 << " seconds\n";
   } else if(safetyspec.parse(input)){
     common_verifier = &k_induction_verifier;
     trace.setVerifier(&k_induction_verifier);
     length.setVerifier(&k_induction_verifier);
+    auto start = std::chrono::high_resolution_clock::now();
     std::string result = safetyspec.perform();
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
     std::cout << result << std::endl;
+    std::cout << "Time taken: " << duration.count()/1e6 << " seconds\n";
   } else {
     std::cout << "Invalid command\n";
   }
