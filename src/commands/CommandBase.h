0d2fc71e8bfe56aabd2d4dbcb13a340cdbe2b4a4
Anish Badri R S <anishbadhri@gmail.com>
2020-04-20 19:33:24 +0530

    Modified CommandBase to used shared pointers, changes to allow CommandBase to copy by value, change printer function to const reference parameters

--- src/commands/CommandBase.h
+++ src/commands/CommandBase.h
@@ -13,110 +13,87 @@ class CommandBase {
       std::vector<Symbol> symbols;
       Kripke kripke;
       std::map<std::string,std::string> label_mapper;
-      CommandBaseSignature(std::vector<Symbol> s, Kripke k, std::map<std::string,std::string> labelMapper)
-              :symbols(std::move(s)), kripke(std::move(k)), label_mapper(std::move(labelMapper)){}
+      CommandBaseSignature(std::vector<Symbol> s, Kripke k, std::map<std::string,std::string> labelMapper): symbols(std::move(s)), kripke(std::move(k)), label_mapper(std::move(labelMapper)){}
     };
-    CommandBase(const std::vector<Symbol>& symbols,Kripke kripke,const std::map<std::string,std::string>& labelMapper)
-    : signature(symbols, std::move(kripke), labelMapper),
-      ltl_bmc_verifier(signature.symbols,signature.kripke.getI(),signature.kripke.getT()),
-      k_induction_verifier(signature.symbols,signature.kripke.getI(),signature.kripke.getT()),
-      command_bound(ltl_bmc_verifier),
-      command_length(*common_verifier),
-      command_ltlspec(ltl_bmc_verifier,signature.label_mapper),
-      command_safetyspec(k_induction_verifier,signature.label_mapper),
-      command_trace(*common_verifier)
-      {}
-    explicit CommandBase(const CommandBaseSignature& commandBaseSignature)
-      : CommandBase(commandBaseSignature.symbols, commandBaseSignature.kripke, commandBaseSignature.label_mapper){}
+
+    CommandBase(const std::vector<Symbol>& symbols,Kripke kripke,const std::map<std::string,std::string>& labelMapper);
+    explicit CommandBase(const CommandBaseSignature& commandBaseSignature): CommandBase(commandBaseSignature.symbols, commandBaseSignature.kripke, commandBaseSignature.label_mapper){}
     CommandBase(const CommandBase &commandBase): CommandBase(commandBase.getSignature()) {};
-    CommandResponse perform(const std::string &line);
+
+    [[nodiscard]] CommandResponse perform(const std::string &line) const;
     [[nodiscard]] inline CommandBaseSignature getSignature() const {
       return signature;
     }
-    inline void setModel(CommandBaseSignature &commandBaseSignature) {
-      signature = commandBaseSignature;
-      ltl_bmc_verifier = ltlBmc(signature.symbols,signature.kripke.getI(),signature.kripke.getT());
-      k_induction_verifier = kInduction(signature.symbols, signature.kripke.getI(), signature.kripke.getT());
-      command_bound = CommandBound(ltl_bmc_verifier);
-      command_length = CommandLength(*common_verifier);
-      command_ltlspec = CommandLtlspec(ltl_bmc_verifier,signature.label_mapper);
-      command_safetyspec = CommandSafetyspec(k_induction_verifier,signature.label_mapper);
-      command_trace = CommandTrace(*common_verifier);
-    }
+
   private:
-    CommandBaseSignature signature;
-    ltlBmc ltl_bmc_verifier;
-    kInduction k_induction_verifier;
-    Verifier *common_verifier = nullptr;
     class CommandInterface {
       public:
-        explicit CommandInterface(std::string op): operation(std::move(op)){}
+        CommandInterface(std::string op, std::shared_ptr<ltlBmc> lbv,
+                         std::shared_ptr<kInduction> kiv);
+        virtual void setVerifier(const std::shared_ptr<Verifier> &v){}
+        inline std::string getOperation() { return operation;}
+        virtual inline std::string getMessage(){ return std::__cxx11::string();};
         virtual bool parse(std::string line) = 0;
         virtual int perform() = 0;
-        inline std::string getOperation() { return operation;}
-        virtual inline std::string getMessage(){ return std::__cxx11::string(); };
+      protected:
+        std::shared_ptr<ltlBmc> ltl_bmc_verifier;
+        std::shared_ptr<kInduction> k_induction_verifier;
+        std::shared_ptr<Verifier> common_verifier;
       private:
         std::string operation;
     };
     class CommandBound : public CommandInterface {
       public:
-        explicit CommandBound(ltlBmc &lB);
+        CommandBound(std::vector<std::shared_ptr<CommandInterface>> cmd, std::shared_ptr<ltlBmc> lbv, std::shared_ptr<kInduction> kiv);
         bool parse(std::string line) override;
         int perform() override;
       private:
-        ltlBmc *ltl_bmc_verifier;
         int bound;
     };
     class CommandLength : public CommandInterface {
       public:
-        explicit CommandLength(Verifier &v);
-        void setVerifier(Verifier *v);
+        CommandLength(std::vector<std::shared_ptr<CommandInterface>> cmd, std::shared_ptr<ltlBmc> lbv, std::shared_ptr<kInduction> kiv);
+        void setVerifier(const std::shared_ptr<Verifier> &v) override;
         bool parse(std::string line) override;
         int perform() override;
-      private:
-          Verifier *verifier;
     };
     class CommandLtlspec : public CommandInterface {
       public:
-        CommandLtlspec(ltlBmc &a,std::map<std::string, std::string> labelMapper);
+        CommandLtlspec(std::vector<std::shared_ptr<CommandInterface>> cmd, std::shared_ptr<ltlBmc> lbv, std::shared_ptr<kInduction> kiv, std::map<std::string, std::string> labelMapper);
         bool parse(std::string line) override;
         int perform() override;
       private:
-        ltlBmc *ltl_bmc_verifier;
         std::map<std::string, std::string> label_mapper;
         std::string property;
     };
     class CommandQuit : public CommandInterface {
       public:
-        CommandQuit();
+        CommandQuit(std::vector<std::shared_ptr<CommandInterface>> cmd, std::shared_ptr<ltlBmc> lbv, std::shared_ptr<kInduction> kiv);
         bool parse(std::string line) override;
         int perform() override;
     };
     class CommandSafetyspec : public CommandInterface {
       public:
-        CommandSafetyspec(kInduction &a, std::map<std::string, std::string> labelMapper);
+        CommandSafetyspec(std::vector<std::shared_ptr<CommandInterface>> cmd, std::shared_ptr<ltlBmc> lbv, std::shared_ptr<kInduction> kiv, std::map<std::string, std::string> labelMapper);
         bool parse(std::string line) override;
         int perform() override;
       private:
-        kInduction *k_induction_verifier;
         std::map<std::string, std::string> label_mapper;
         std::string property;
     };
     class CommandTrace : public CommandInterface {
       public:
-        explicit CommandTrace(Verifier &v);
-        void setVerifier(Verifier *v);
+        CommandTrace(std::vector<std::shared_ptr<CommandInterface>> cmd, std::shared_ptr<ltlBmc> lbv, std::shared_ptr<kInduction> kiv);
+        void setVerifier(const std::shared_ptr<Verifier> &v) override;
         bool parse(std::string line) override;
         int perform() override;
         std::string getMessage() override;
-    private:
-        Verifier *verifier;
     };
 
-    CommandBound command_bound;
-    CommandLength command_length;
-    CommandLtlspec command_ltlspec;
-    CommandQuit command_quit;
-    CommandSafetyspec command_safetyspec;
-    CommandTrace command_trace;
+    CommandBaseSignature signature;
+    std::vector<std::shared_ptr<CommandInterface>> command;
+    enum command_list{BOUND, LENGTH, LTLSPEC, QUIT, SAFETYSPEC, TRACE};
+
+    std::shared_ptr<ltlBmc> ltl_bmc_verifier;
+    std::shared_ptr<kInduction> k_induction_verifier;
 };
--- src/interpreter/Interpreter.h
+++ src/interpreter/Interpreter.h
@@ -8,10 +8,10 @@
 
 class Interpreter {
   CommandBase command_base;
-  std::function<void(CommandResponse,CommandBase&)> print_function;
+  std::function<void(const CommandResponse&,const CommandBase&)> print_function;
 public:
-  explicit Interpreter(CommandBase &commandBase): command_base(commandBase) {}
-  void setPrinter(std::function<void(CommandResponse,CommandBase&)> printer){
+  explicit Interpreter(CommandBase commandBase): command_base(std::move(commandBase)) {}
+  void setPrinter(std::function<void(const CommandResponse&,const CommandBase&)> printer){
     print_function = std::move(printer);
   }
   void interpret(const std::string& input) {
